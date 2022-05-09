dada2cefbfeff5947a436d644c021bc7581a3391
wmo-grdc <104775632+wmo-grdc@users.noreply.github.com>
2022-05-09 17:50:09 +0200

    Update install.R

--- install.R
+++ install.R
@@ -1,1 +1,3 @@
 install.packages("dplyr")
+install.packages("hyddate",repos="ftp://ftp.bafg.de/pub/REFERATE/GRDC/R", type="source")
+install.packages("hydts",repos="ftp://ftp.bafg.de/pub/REFERATE/GRDC/R", type="source")
