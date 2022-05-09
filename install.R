0f4a5757ebb71a99506e213feac5a7fa5a339f02
wmo-grdc <104775632+wmo-grdc@users.noreply.github.com>
2022-05-09 19:55:03 +0200

    Update install.R

--- install.R
+++ install.R
@@ -1,4 +1,4 @@
 install.packages("dplyr")
-install.packages(c("abind", "lubridate", "ncdf4", "xts", "zoo")
+install.packages(c("abind", "lubridate", "ncdf4", "xts", "zoo"))
 install.packages("hyddate",repos="http://116.203.125.251/drat", type="source")
 install.packages("hydts",repos="http://116.203.125.251/drat", type="source")
