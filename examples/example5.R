af67278d47e28fbc64f14efb4006e9fe10c29cfd
Thomas Recknagel <recknagel@bafg.de>
2022-11-08 17:15:49 +0100

    ok

--- examples/example5.R
+++ examples/example5.R
@@ -6,11 +6,11 @@ watergap <- raster::stack("example.nc4")
 
 names(watergap) <- 1901:2016
 
-watergap.jan1901 <- watergap$X1901
+watergap.1901 <- watergap$X1901
 
-plot(watergap.jan1901)
-raster::values(watergap.jan1901) <- log(raster::values(watergap.jan1901))
-plot(watergap.jan1901)
+plot(watergap.1901)
+raster::values(watergap.1901) <- log(raster::values(watergap.1901))
+plot(watergap.1901)
 plot(st_geometry(catchment), add=TRUE)
 
 kongo.discharge <- raster::crop(watergap, catchment)
--- examples/example6.R
+++ examples/example6.R
@@ -3,16 +3,16 @@ cell <- raster::cellFromXY(kongo.discharge, xy = c(15.3,-4.3))
 kinshasa.cell <- kongo.discharge[cell]
 
 
-watergap.kinshasa.yearly <- hydts::hydtsReg(startDate = as.POSIXct("1901-01-01", tz="GMT"),
+watergap.kinshasa.yearlymean <- hydts::hydtsReg(startDate = as.POSIXct("1901-01-01", tz="GMT"),
                                              timestep = "1 year",
                                              data = unname(kinshasa.cell[1,]))
 
-plot(watergap.kinshasa.yearly)
+plot(watergap.kinshasa.yearlymean)
 lines(kongo.yearlymean,col="blue")
 
-names(watergap.kinshasa.yearly) <- "kinshasa model"
+names(watergap.kinshasa.yearlymean) <- "kinshasa model"
 names(kongo.yearlymean) <- "kinshasa obs"
-ts <- merge(watergap.kinshasa.yearly, kongo.yearlymean)
+ts <- merge(watergap.kinshasa.yearlymean, kongo.yearlymean)
 
 plot(ts)
 dygraphs::dygraph(ts)
